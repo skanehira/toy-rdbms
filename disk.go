a030e312e7b6dcc27b44facf76438f1052ade3e9
skanehira <sho19921005@gmail.com>
2021-04-24 08:41:32 +0900

    implementing disk manager

---
+++ disk.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"os"
+)
+
+const (
+	PAGE_SIZE = 4096
+)
+
+type PageID uint64
+
+type DiskManager struct {
+	HeapFile   *os.File
+	NextPageID PageID
+}
+
+// New 新しいDiskManagerを作成
+func New() *DiskManager {
+	m := &DiskManager{}
+	return m
+}
+
+// Open ファイルを開く
+func (d *DiskManager) Open(path string) error {
+	f, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+
+	d.HeapFile = f
+
+	i, err := f.Stat()
+	if err != nil {
+		return err
+	}
+
+	d.NextPageID = PageID(i.Size() / PAGE_SIZE)
+
+	return nil
+}
+
+// AllocatePage 新しいページIDを採番する
+func (d *DiskManager) AllocatePage() PageID {
+	d.NextPageID++
+	return d.NextPageID
+}
+
+// Read ページデータを読み出す
+func (d *DiskManager) Read(pageID PageID, data *[8]byte) error {
+	offset := int64(PAGE_SIZE * uint64(pageID))
+	_, err := d.HeapFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+	_, err = d.HeapFile.Read(data[:])
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// Write ページデータに書き出す
+func (d *DiskManager) Write(pageID PageID, data [8]byte) error {
+	offset := int64(PAGE_SIZE * uint64(pageID))
+	_, err := d.HeapFile.Seek(offset, 0)
+	if err != nil {
+		return err
+	}
+	_, err = d.HeapFile.Write(data[:])
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/skanehira/toy-rdbms
+
+go 1.16
